2d3ddaef33697d6cf55447802ef2b7cd4bfc3ee2
Jason Allum <jallum@tonicdesign.com>
2018-06-27 10:49:02 -0400

    Added gitignore; dependencies.

--- Package.swift
+++ Package.swift
@@ -3,12 +3,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "Clibopencv",
-    pkgConfig: "opencv",
-    providers: [
-        .apt(["libopencv-dev"])
-    ],
-    products: [ .library(name: "Clibopencv", targets: ["Clibopencv"]) ],
-    dependencies: [
-    ]
+  name: "Clibopencv",
+  pkgConfig: "opencv",
+  providers: [
+    .apt(["libopencv-dev"])
+  ],
+  products: [ .library(name: "Clibopencv", targets: ["Clibopencv"]) ],
+  dependencies: [
+    .package(url: "https://github.com/theia-ai/Clibfreetype.git", .branch("master")),
+    .package(url: "https://github.com/theia-ai/Clibharfbuzz.git", .branch("master")),
+  ],
+  targets: [
+    .target(
+      name: "Clibopencv",
+      dependencies: [
+        "Clibharfbuzz",
+        "Clibfreetype"
+      ]
+    )
+  ]
 )

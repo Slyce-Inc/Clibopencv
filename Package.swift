13c93071a7bd48adb3327b8d976e07624722adde
Jason Allum <jason@slyce.it>
2020-01-28 15:53:01 +0000

    Updated to use the newest version of OpenCV available.

--- Package.swift
+++ Package.swift
@@ -1,6 +1,22 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 
 import PackageDescription
+
+#if os(Linux)
+import Foundation
+
+let fileManager = FileManager.default
+guard let pcFileToUse = (try! fileManager.contentsOfDirectory(atPath:"/usr/lib/pkgconfig")
+  .filter { $0.hasPrefix("opencv") && $0.hasSuffix(".pc") }
+  .sorted()
+  .last) else {
+  fatalError("Unable to locate a .pc for opencv")
+}
+let versionToUse = NSURL(fileURLWithPath:pcFileToUse).deletingPathExtension!.lastPathComponent
+#else
+let versionToUse = "opencv"
+#endif
+
 
 let package = Package(
   name: "Clibopencv",
@@ -13,7 +29,7 @@ let package = Package(
     .package(url: "https://github.com/Slyce-Inc/Clibharfbuzz.git", .branch("master")),
   ],
   targets: [
-    .systemLibrary(name:"Clibopencv", pkgConfig: "opencv4"),
+    .systemLibrary(name:"Clibopencv", pkgConfig: versionToUse),
     .target(
       name: "OpenCV",
       dependencies: [
